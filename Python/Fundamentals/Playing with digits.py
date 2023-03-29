1a6b17978faa78fd3f2483b21919decc2856d150
Vladislav <vladislav_semykin01@mail.ru>
2023-03-29 13:11:21 +0300

    6 kyu. Added smart solution

--- Python/Fundamentals/Playing with digits.py
+++ Python/Fundamentals/Playing with digits.py
@@ -10,8 +10,22 @@ def dig_pow(n, p):
     return -1
 
 
+def dig_pow_clever(n, p):
+    s = 0
+    for i, c in enumerate(str(n)):
+        s += pow(int(c), p+i)
+    return s/n if s % n == 0 else -1
+
+
 print(dig_pow(89, 1))
 print(dig_pow(92, 1))
 print(dig_pow(695, 2))
 print(dig_pow(46288, 3))
 print(dig_pow(46288, 5))
+
+print()
+print(dig_pow_clever(89, 1))
+print(dig_pow_clever(92, 1))
+print(dig_pow_clever(695, 2))
+print(dig_pow_clever(46288, 3))
+print(dig_pow_clever(46288, 5))

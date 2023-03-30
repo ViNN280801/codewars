34baea695c443af31d0c3eb8d96db49faac1e1b6
Vladislav <vladislav_semykin01@mail.ru>
2023-03-30 10:46:43 +0300

    7 kyu

---
+++ C++/Fundamentals/Count the Digits.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <iterator>
+#include <string>
+#include <algorithm>
+#include <cmath>
+
+class CountDig
+{
+public:
+    static int nbDig(int n, int d)
+    {
+        std::string str{};
+        for (int i{0}; i <= n; i++)
+        {
+            str += std::to_string(static_cast<int>(std::pow(static_cast<long double>(i), 2.0)));
+        }
+        return std::count_if(std::cbegin(str), std::cend(str), [&](char const &ch)
+                             { return ch == (d + '0'); });
+    }
+
+    static int nbDig_clever(int n, int d)
+    {
+        std::string digits;
+        for (int k = 0; k <= n; ++k)
+            digits += std::to_string(k * k);
+
+        return std::count(digits.begin(), digits.end(), std::to_string(d)[0]);
+    }
+};
+
+int main()
+{
+    std::cout << CountDig::nbDig(10, 1) << std::endl;
+    std::cout << CountDig::nbDig(25, 1) << std::endl;
+    std::cout << CountDig::nbDig(5750, 0) << std::endl;
+
+    std::cout << CountDig::nbDig_clever(10, 1) << std::endl;
+    std::cout << CountDig::nbDig_clever(25, 1) << std::endl;
+    std::cout << CountDig::nbDig_clever(5750, 0) << std::endl;
+
+    return EXIT_SUCCESS;
+}

c89fc2fc69f87d9b02fecae780a5fa15655bd891
Vladislav <vladislav_semykin01@mail.ru>
2023-06-14 22:15:07 +0300

    7 kyu

--- Python/Fundamentals/Testing 1-2-3.py
+++ Python/Fundamentals/Testing 1-2-3.py
@@ -4,4 +4,9 @@ def number(lines: list) -> list:
     return lines
 
 
+def number_smart(lines: list) -> list:
+    return ['%d: %s' % v for v in enumerate(lines, 1)]
+
+
 print(number(['a', 'b', 'c']))
+print(number_smart(['a', 'b', 'c']))

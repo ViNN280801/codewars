db7c34b8c63d83acf8001343c1dbbe0ba48a4595
Vladislav <vladislav_semykin01@mail.ru>
2023-09-03 16:25:40 +0300

    6 kyu

---
+++ Python/Fundamentals/Multiptiplication table.py
@@ -0,0 +1,26 @@
+def multiplication_table(size: int) -> list[list[int]]:
+    """ 
+    Creates a multiplication table of the given size
+
+    Args:
+        size(int): The size of the table (it should be a square table)
+
+    Returns: 
+        Multiplication table n x n size 
+    """
+
+    result = [[0 for _ in range(size)] for _ in range(size)]
+
+    # Filling first horizontal and vertical values of a multiplication table
+    for i in range(size):
+        result[i][0] = result[0][i] = i + 1
+
+    # Filling the remaining elements
+    for i in range(size):
+        for j in range(size):
+            result[i][j] = result[0][j] * result[i][0]
+
+    return result
+
+
+print(multiplication_table(3))
